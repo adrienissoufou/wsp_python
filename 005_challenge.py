28649484810600cc58afa07f8d95203af95d75a8
adrienIssoufou <adrienissoufou@gmail.com>
2022-09-18 12:44:28 +0200

    challenge 005

---
+++ 005_challenge.py
@@ -0,0 +1,16 @@
+#challenge 05
+#Ask the user to enter three numbers.
+# Add together the first two numbers 
+# and then multiply this total by third.
+# Display the answer as The answer is [answer]
+
+print ("------ Hello --------")
+nbr1 = int(input("enter number 1 : "))
+nbr2 = int(input("enter number 2 : "))
+nbr3 = int(input("enter number 3 : "))
+
+som = nbr1 + nbr2 
+mult = som * nbr3
+
+print("The answer is :  ", mult)
+
