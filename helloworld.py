4fe96238d46c77607d11875e609f1dde4dce615f
adrienIssoufou <adrienissoufou@gmail.com>
2022-09-18 11:11:34 +0200

    helloWorld

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+print("Hello World !\n")
+print("adrien")
+cpt = input("entrez nim :")
+addition = 3 + 3
+soustraction = 3 - 3
+print("Salut ", cpt)
+print ("addition = ",addition)
+print ("soustraction = ", soustraction)
+age = int(input("intro age : "))
+print("vous aves ",age ,"ans")
