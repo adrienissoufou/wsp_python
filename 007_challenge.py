9035e354acf9ea34ac389478538251becf62cf57
adrienIssoufou <adrienissoufou@gmail.com>
2022-09-18 13:12:14 +0200

    challenge 008

---
+++ 007_challenge.py
@@ -0,0 +1,9 @@
+#challenge 007
+# Ask the user for their name their age.
+# Add 1 to their age and display the output
+# [Name] next birthday you will be [new age]
+
+name = input("your name please :")
+age  = int(input("your age : "))
+age = age + 1
+print(name, "next birthday you will be", age , ".")
