52fd8c17214ed595ae4a2ffb9c7dea50e009f952
adrienIssoufou <adrienissoufou@gmail.com>
2022-09-18 11:50:11 +0200

    challenge 003 enonce

--- 003_challenge.py
+++ 003_challenge.py
@@ -1,1 +1,9 @@
+#challenge 003
+#  Write code that will display the joke 
+#  "What do you call a bear with no teeth?" 
+#  and on the next line display the answer "
+#  A gummy bear ! " 
+#  try to create it using only line of code.
+
+
 print("What do you call a bear with no teeth? \nA gummy bear!")
