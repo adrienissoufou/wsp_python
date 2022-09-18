648bde13a59854056ad8abead7e34f0b3617c570
adrienIssoufou <adrienissoufou@gmail.com>
2022-09-18 12:02:42 +0200

    challenge 004

---
+++ 004_challenge.py
@@ -0,0 +1,11 @@
+#challenge 004
+#Ask the user to enter two numbers.
+#Add them together and display the answer as
+#The total is
+#[answer]
+
+print (" ---- Add ")
+nbr1 = int(input("intro nbr 1  : "))
+nbr2 = int(input("intro nbr 2  : "))
+answer = nbr1 + nbr2
+print("The total is \n", answer)
