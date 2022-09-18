bd4adca8e0798e91a0600644c8b14e1132fd10c8
adrienIssoufou <adrienissoufou@gmail.com>
2022-09-18 18:10:54 +0200

    string exemple

---
+++ string/exemple.py
@@ -0,0 +1,41 @@
+print(" new lign ")
+
+print("Prints the values to a stream, or to sys.stdout by default. Optional keyword arguments:")
+
+print("""Prints
+        the values 
+        to a stream,
+        or to sys
+        . stdout by default. 
+        Optional 
+        keyword arguments:""")
+
+print("Prints \nthe values \nto a stream, or to sys.\nstdout by default. Optional\n keyword arguments:")
+
+print(" len : compte char")
+print("adrien nbr Caractere ",len("adrien") )
+
+print(" ---- word.upper ------ ")
+print("adrien : ","adrien".upper() )
+print("adrien : ",str.upper("adrien") )
+
+
+print(" ---- word.lower ------ ")
+print("AdrIen : ","AdrIen".lower() )
+print("AdrIen : ",str.lower("AdrIen") )
+
+print(" ---- word.capitalize ------ ")
+print("adrien : ","adrien".capitalize() )
+print("adrien : ",str.capitalize("adrien") )
+
+print(" ---- word.title ------ ")
+print("adrien ISSOUFOU : ","adrien ISSOUFOU".title() )
+print("adrien ISSOUFOU : ",str.title("adrien ISSOUFOU") )
+
+print(" ---- Concatenation ------ ")
+print("adrien ISSOUFOU : ","adrien" + "ISSOUFOU" )
+print("adrien ISSOUFOU : ","adrien" + "ISSOUFOU") 
+
+
+print(" ---- strip : espace devant devant et devant ------ ")
+print("            adrien ISSOUFOU  \n", " adrien ISSOUFOU ".strip("") )
