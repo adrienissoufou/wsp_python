60ddb7c50f7131684d83af942047d7cc9bc3c013
adrienIssoufou <adrienissoufou@gmail.com>
2022-09-18 14:40:55 +0200

    exemple 2 if imbriquer

---
+++ if/exemple2.py
@@ -0,0 +1,9 @@
+num = int (input("enter nbr : "))
+
+if num >= 10:
+    if num <=20:
+        print("This is between 10 and 20")
+    else:
+        print("this is over 20")
+else:
+    print("This is under 10")
