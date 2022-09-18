ec4e44397cc1f26ca09231dab0f2307e3ec02928
adrienIssoufou <adrienissoufou@gmail.com>
2022-09-18 12:57:37 +0200

    challenge 006

---
+++ 006_challenge.py
@@ -0,0 +1,10 @@
+#challenge 006
+# Ask how many slices of pizza the user started with
+# and ask how many slices the have eaten.
+# Work out how many slices they have left
+# and display the answer in a user-friendly form.
+
+print("-------- HEllo -----------")
+total = int(input("how many slices of pizza, you have ? : " ))
+eaten = int(input("How many slices, have you eat ? : "))
+print("you have left ",total-eaten," slices of pizza.")
