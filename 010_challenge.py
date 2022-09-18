709e0383ec0e8d95d855210d43950188579d52c8
adrienIssoufou <adrienissoufou@gmail.com>
2022-09-18 13:55:56 +0200

    challenge 010

---
+++ 010_challenge.py
@@ -0,0 +1,11 @@
+# challenge 010
+# There are 2,204 pounds in a kilogram.
+# Ask the user to enter a weight 
+# in kilograms and convert it to pounds.
+
+print("------ Hello ------")
+nbr_kilogram = int(input("enter Kilograms? : "))
+
+nbr_pounds = nbr_kilogram * 2204
+
+print(" you have : ", nbr_pounds ," pounds.")
